821bc9daf040a112eb445309bb8bdb774ec1c9fd
Dzhake <andryzindmi@gmail.com>
2026-05-26 15:56:09 +0300

    Use MLEM's nuget package

---
+++ src/Main/Utils/UnsafeUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Monod.Utils;
+
+public static class UnsafeUtils
+{
+    /// <summary>
+    /// Get field of the <paramref name="obj"/> offset by <paramref name="offset"/>.
+    /// </summary>
+    /// <typeparam name="TStruct">Type of the struct, which contains the field.</typeparam>
+    /// <typeparam name="TField">Type of the object stored in field.</typeparam>
+    /// <param name="obj">Struct, which contains the field.</param>
+    /// <param name="offset">Offset of the field relative to struct.</param>
+    /// <returns>Value of the field.</returns>
+    public static ref TField GetField<TStruct, TField>(ref TStruct obj, IntPtr offset) where TStruct : struct => ref Unsafe.As<byte, TField>(ref Unsafe.AddByteOffset(ref Unsafe.As<TStruct, byte>(ref obj), offset));
+
+    public static nint GetFieldOffset<TStruct, TField>(ref TStruct obj, ref TField field) => Unsafe.ByteOffset(ref Unsafe.As<TStruct, byte>(ref obj), ref Unsafe.As<TField, byte>(ref field));
+}
