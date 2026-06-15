d2c99241422c7569ed2e0dbfbad07bbb9a772327
Dzhake <andryzindmi@gmail.com>
2026-06-15 19:53:25 +0300

    Kinda fix weird flip on edge bug

--- src/Main/MathModule/MathM.cs
+++ src/Main/MathModule/MathM.cs
@@ -8,5 +8,19 @@ public static class MathM
     /// <summary>
     /// A small value, by default 0.001.
     /// </summary>
-    public static readonly float Epsilon = 0.001f;
+    public static readonly float Epsilon = 0.000001f;
+
+    public static void LerpFloat(ref float from, float to, float amount)
+    {
+        if (from > to)
+        {
+            from -= amount;
+            if (from < to) from = to;
+        }
+        else if (from < to)
+        {
+            from += amount;
+            if (from > to) from = to;
+        }
+    }
 }
