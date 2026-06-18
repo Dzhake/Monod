f401d34444102271cd54eecf9c3bb26bb36e58b7
Dzhake <andryzindmi@gmail.com>
2026-06-18 20:56:11 +0300

    Add bouncing off bouncy entities

--- src/Main/MathModule/MathM.cs
+++ src/Main/MathModule/MathM.cs
@@ -5,8 +5,13 @@
 /// </summary>
 public static class MathM
 {
+    public static Vector2 VectorUp = new(0, -1);
+    public static Vector2 VectorDown = new(0, 1);
+    public static Vector2 VectorRight = new(1, 0);
+    public static Vector2 VectorLeft = new(-1, 0);
+
     /// <summary>
-    /// A small value, by default 0.001.
+    /// A small value.
     /// </summary>
     public static readonly float Epsilon = 0.000001f;
 
