344d61328aff1b8aa0cd4e428fe80f3826dd631f
Dzhake <andryzindmi@gmail.com>
2026-07-01 01:22:33 +0300

    Start implementing prefabs

---
+++ src/Main/ECS/EntityExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Monod.ECS;
+
+public static class EntityExtensions
+{
+    public static void RemoveComponentByType(this ref Entity entity, Type componentType)
+    {
+        EntitySchema schema = EntityStore.GetEntitySchema();
+        var componentInternalType = schema.ComponentTypeByType[componentType];
+        EntityUtils.RemoveEntityComponent(entity, componentInternalType);
+    }
+
+    public static void AddOrChangeComponent(this ref Entity entity, IComponent component)
+    {
+        EntitySchema schema = EntityStore.GetEntitySchema();
+        var componentType = schema.ComponentTypeByType[component.GetType()];
+        EntityUtils.AddEntityComponent(entity, componentType);
+        EntityUtils.AddEntityComponentValue(entity, componentType, component);
+    }
+}
---
+++ src/Main/ECS/Prefabs/Prefab.cs
@@ -0,0 +1,74 @@
+﻿using Monod.AssetsModule;
+using System.Text.Json.Serialization;
+
+namespace Monod.ECS.Prefabs;
+
+public abstract class Prefab
+{
+    public abstract Entity Instantiate(EntityStore store);
+}
+
+// if the class is ever unsealed, implement a proper dispose pattern.
+public sealed class EntityPrefab : Prefab, IDisposable
+{
+    public Entity Source;
+
+    public override Entity Instantiate(EntityStore store)
+    {
+        Entity target = store.CreateEntity();
+        Source.CopyEntity(target);
+        return target;
+    }
+
+    public void Dispose()
+    {
+        Source.DeleteEntity();
+    }
+}
+
+// if the class is ever unsealed, implement a proper dispose pattern.
+public sealed class ModificationPrefab : Prefab, IDisposable
+{
+    public string BasePrefabPath;
+    public Entity? Source;
+    public Type[]? RemoveComponents;
+
+    [JsonIgnore]
+    public Prefab? BasePrefab;
+
+    public ModificationPrefab(string basePrefabName, Entity? source = null, Type[]? removeComponents = null)
+    {
+        BasePrefabPath = basePrefabName;
+        Source = source;
+        RemoveComponents = removeComponents;
+        Assets.OnReload += LoadAssets;
+        LoadAssets();
+    }
+
+    public void LoadAssets()
+    {
+        BasePrefab = Assets.GetOrDefault<Prefab>(BasePrefabPath);
+    }
+
+    public override Entity Instantiate(EntityStore store)
+    {
+        Entity entity = BasePrefab?.Instantiate(store) ?? store.CreateEntity();
+
+        if (RemoveComponents is not null)
+        {
+            foreach (var componentTypeToRemove in RemoveComponents)
+                entity.RemoveComponentByType(componentTypeToRemove);
+        }
+
+        if (Source is not null)
+            EntityStore.MergeEntity(Source.Value, entity);
+
+        return entity;
+    }
+
+    public void Dispose()
+    {
+        Assets.OnReload -= LoadAssets;
+        Source?.DeleteEntity();
+    }
+}
