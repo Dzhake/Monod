9d5be54fa4d2773a72d7c0025900a7080160ef78
Dzhake <andryzindmi@gmail.com>
2026-07-03 14:57:31 +0300

    Make bunny detect player

--- src/Main/Utils/Extensions/Vector2Extensions.cs
+++ src/Main/Utils/Extensions/Vector2Extensions.cs
@@ -2,7 +2,7 @@
 
 public static class Vector2Extensions
 {
-    public static void NormalizeSafe(this Vector2 v)
+    public static void NormalizeSafe(this ref Vector2 v)
     {
         if (v == Vector2.Zero) return;
         v.Normalize();
